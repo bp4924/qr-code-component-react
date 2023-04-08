6ded83caabdb8f6e61458f2b69223b6afb8b3852
Bernie Petters <bp4924@gmail.com>
2023-04-08 12:33:58 -0400

    Add html to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from "./logo.svg";
 import "./style.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="container">
+          <img src="./images/image-qr-code.png" alt="qr code link" />
+          <h1>Improve your front-end skills by building projects!</h1>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
       </header>
+      <div class="attribution">
+        Challenge by
+        <a
+          rel="noreferrer"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Bernie Petters</a>.
+      </div>
     </div>
   );
 }
